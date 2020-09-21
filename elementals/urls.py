62176209747ac460b21abd3679525e46eb2f1dfc
josh <josh@debian.work.lan>
2020-09-21 16:25:29 -0400

    Basic front page

--- elementals/urls.py
+++ elementals/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from dgango.views.generic import TemplateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="home.html")),
 ]
