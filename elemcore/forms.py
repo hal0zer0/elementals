c3e68e22f60b74264e3725335fd47a93c065c0fb
Joshua Price <price.joshuad@gmail.com>
2020-10-06 12:19:25 -0400

    Some excellent progress on the card creator

--- elemcore/forms.py
+++ elemcore/forms.py
@@ -1,7 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 from elemcore import models as emodels
 
-class ConstructForm(ModelForm):
+class ConstructForm(forms.ModelForm):
+    subtype = forms.CharField(max_length=16)
+
+    def save(self, commit=True):
+        subtype_name = self.cleaned_data['subtype']
+        subtype = emodels.CardSubtype.objects.get_or_create(name=subtype_name)[0]
+        self.instance.subtype = subtype
+
     class Meta:
         model = emodels.Construct
-        fields = ['name', 'attack', 'defense']
+        fields = ['name', 'attack', 'defense', 'picture', 'flavor_text', 'rarity', 'public', 'user'] # User is hidden
+        widgets = {'user': forms.HiddenInput()}
--- elementals/views/create.py
+++ elementals/views/create.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from elemcore import models as emodels
+from django.contrib import messages
 from elemcore import forms
 
 def show(request):
-    form = forms.ConstructForm()
-    return render(request, 'create_card.html', {'form': form})
+    context = {}
+    form = forms.ConstructForm(request.POST or None, initial={'user': request.user})
+    if form.is_valid():
+        #form.cleaned_data["user_id"] = request.user.id
+        form.save()
+        messages.add_message(request, messages.INFO, 'Card Created')
+
+    context['form'] = form
+    return render(request, 'create_card.html', context)
